a03a46b729bb328e86b79011c2410451741af23d
irwandi010809 irwan <irwandi2266@gmail.com>
2024-08-04 04:37:24 +0000

    update

--- utils/deployed-address.ts
+++ utils/deployed-address.ts
@@ -1,4 +1,4 @@
-export const SWTRProxy = '0xea65D3f80d2253293a64e2f8DdF4CEC401823053'
-export const ProxyAdmin = '0x6a0baE5CF96d14e39FA9F5AdB4fadABb55B22E17'
-export const SWTRImplementation = '0xE4e93a474734E22A4f660EF1fE7aC3fc096E856c'
+export const SWTRProxy = '0x7296410aDb818D90e3e3Ab9e3df8211defa051d7'
+export const ProxyAdmin = '0xc5868C41aBa5A148F5db4D56CD696D58B23CeF81'
+export const SWTRImplementation = '0x69Fd592D638C36FbE286b568EA4Be691a2C6501a'
     
